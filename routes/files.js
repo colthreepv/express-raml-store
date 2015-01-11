dcfa2ccf8ea7172946d2895f2ced0b5e303b64ea
Arthur Tsang <chiuwai.tsang@hp.com>
2015-01-10 18:36:00 -0800

    feat(api): implement api required by api-designer to store RAML files to filesystem

--- routes/files.js
+++ routes/files.js
@@ -1,125 +1,141 @@
-var mongo = require('mongodb');
- 
-var Server = mongo.Server,
-Db = mongo.Db,
-BSON = mongo.BSONPure;
- 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('ramldb', server);
- 
-db.open(function(err, db) {
-if(!err) {
-console.log("Connected to 'ramldb' database");
-db.collection('files', {strict:true}, function(err, collection) {
-if (err) {
-console.log("The 'files' collection doesn't exist. Use POST to add RAML files...");
-populateDB();
-}
-});
-}
-});
- 
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving file: ' + id);
-    db.collection('files', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-            delete item._id;
-            res.header("Access-Control-Allow-Origin", "*");
-            res.send(item);
-         });
-     });
+var fs = require('fs');
+require('sugar');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
+var dataPath = process.env.RAML_DATAPATH || './';
+if (!dataPath.endsWith('/')) dataPath += '/';
+
+var walk = function(path, done) {
+  console.log( 'walking ' + path );
+  var dir = dataPath + path;
+  var results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) { 
+      if( err.code === 'ENOTDIR' ) {
+          results.push({
+            path: path,
+            name: require('path').basename(path),
+            type: 'file'
+          });
+          return done(null,results);
+      }
+      else {
+        return done(err);
+      }
+    }
+    var pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(filename) {
+      var file = dir + '/' + filename;
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          var current = {
+            path: path + '/' + filename ,
+            name: filename,
+            type: 'folder'
+          };
+          results.push(current);
+          walk(path+'/'+filename, function(err, res) {
+            //results = results.concat(res);
+            current.children = res;
+            if (!--pending) done(null, results);
+          });
+        } else {
+          console.log( path, filename, file, dir );
+          results.push({
+            path: path + '/' + filename,
+            name: path + '/' + filename,
+            type: 'file'
+            //content: fs.readFileSync(file,'utf-8')
+          });
+          if (!--pending) done(null, results);
+        }
+      });
+    });
+  });
 };
- 
+
 exports.findAll = function(req, res) {
-   var filelist = new Object();
-   db.collection('files', function(err, collection) {
-      collection.find({}, function(err, resultCursor) {
-            resultCursor.each(function(err,item) {
-               if(item != null){
-               console.log('Item : '+item._id+' : ' + JSON.stringify(item));
-               filelist[item._id] = item;
-               delete filelist[item._id]._id;
-               console.log(JSON.stringify(filelist));
-               }
-               else{
-                res.header("Access-Control-Allow-Origin", "*");
-                res.send(JSON.stringify(filelist));
-		}
-            });
-        });
-   });
+  console.log('finding all files');
 
-};
- 
+  var filelist = [];
+  var path = (req.params[0]==='/')?'':req.params[0];
+
+  walk( path, function(err, result) {
+    if (err) {
+      console.log(err);
+      res.send(404);
+    } else {
+      if( result.length == 1 ) {
+        fs.readFile(dataPath + result[0].path, {
+          encoding: 'utf8'
+        }, function(err,content){
+          res.header("Access-Control-Allow-Origin", "*");
+          result[0].content = content;
+          res.send(JSON.stringify(result));
+        })
+      } else {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(JSON.stringify(result));
+      }
+    }
+  });
+}
+
 exports.addFile = function(req, res) {
-    var file = req.body;
-    console.log('Adding file : ' + JSON.stringify(file));
-
-    db.collection('files', function(err, collection) {
-        collection.insert(file, {safe:true}, function(err, result) {
-           if (err) {
-               res.send({'error':'An error has occurred'});
-           } else {
-               console.log('Success: ' + JSON.stringify(result[0]));
-               res.header("Access-Control-Allow-Origin", "*");
-               res.send(result[0]);
-           }
-        });
+  var file = req.body;
+  var path = (req.params[0]==='/')?'':req.params[0];
+  console.log('Creating file: ' + path);
+  if( file.type === 'folder' ) {
+    mkdirp( dataPath + '/' + path, function(err){
+      if(!err){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(201);
+      }
     });
+  } else {
+    var dir = require('path').dirname(path);
+    var name = require('path').basename(path);
+    console.log('Adding file : ' + dir + '===' + path + '--' + JSON.stringify(file));
+    mkdirp('/'+dir, function(err) {
+      fs.writeFile(dataPath+path,unescape(file.content),function(err){
+        if(err){
+          console.log(err);
+          res.send(500);
+        } else {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.send(201);
+        }
+      });
+    });  
+  }
+  
 }
- 
-exports.updateFile = function(req, res) {
-    var id = req.params.id;
-    var file = req.body;
-    console.log('Updating file: ' + id);
-    console.log(JSON.stringify(file));
 
-    db.collection('files', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, file, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating file : ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.header("Access-Control-Allow-Origin", "*");
-                res.send('{"status":"success","id":"'+id+'","message":"The file was successfully updated."}');
-            }
-        });
-    });
-}
- 
 exports.deleteFile = function(req, res) {
-var id = req.params.id;
-console.log('Deleting file: ' + id);
-db.collection('files', function(err, collection) {
-collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-if (err) {
-res.send({'error':'An error has occurred - ' + err});
-} else {
-console.log('' + result + ' document(s) deleted');
-res.send(req.body);
-}
-});
-});
-
-
+  var path = (req.params[0]==='/')?'':req.params[0];
+  console.log('Deleting file: ' + path);
+  rimraf( dataPath + path, function(err){
+    if(err){
+      console.log(err);
+      res.send(500);
+    }else{
+      res.send(200);
+    }
+  });
 }
 
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
- 
-var files = [
-{
-name: "ExampleRAML",
-path: "%2F",
-contents: "gggggg"
-}];
- 
-db.collection('files', function(err, collection) {
-collection.insert(files, {safe:true}, function(err, result) {});
-});
- 
-};
+exports.updateFile = function(req,res) {
+  var path = (req.params[0]==='/')?'':req.params[0];
+  var dest = req.body.newName;
+  console.log('Updating file: ' + path);
+  fs.rename( dataPath+path, dataPath+dest, function(err){
+    if(err){
+      console.log(err);
+      res.send(500);
+    } else {
+      res.send(200);
+    }
+  });
+}
--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-
 var express = require('express'),
-files = require('./routes/files');
- 
+    files = require('./routes/files');
+
 var app = express();
 
-app.configure(function () {
-app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
-app.use(express.bodyParser());
+app.configure(function() {
+    app.set('port', process.env.RAML_PORT || 3000);
+    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.use('/', express.static(__dirname + '/node_modules/api-designer/dist'));
+    app.use(app.router);
 });
 
 app.use(express.methodOverride());
- 
+
 // ## CORS middleware
 //
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
@@ -22,20 +28,17 @@ var allowCrossDomain = function(req, res, next) {
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
         res.send(200);
-    }
-    else {
+    } else {
         next();
     }
 };
 
 app.use(allowCrossDomain);
- 
-app.get('/files', files.findAll);
-app.get('/files/:id', files.findById);
-app.post('/files', files.addFile);
-app.put('/files/:id', files.updateFile);
-app.delete('/files/:id', files.deleteFile);
 
- 
-app.listen(3000);
-console.log('Listening on port 3000...');
+app.get('/files/*', files.findAll);
+app.post('/files/*', files.addFile);
+app.put('/files/*', files.updateFile);
+app.delete('/files/*', files.deleteFile);
+
+app.listen(app.get("port"));
+console.log('Listening on port ' + app.get("port") + '...');
