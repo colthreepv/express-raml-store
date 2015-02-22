af2a9fb0313e49ff2c56eac86a0a20a95dcd2230
mrgamer <mister.gamer@gmail.com>
2015-02-22 14:00:50 +0100

    module.exports now is a function to setup the RAML DATA folder

--- raml.js
+++ raml.js
@@ -1,21 +1,13 @@
-var express = require('express'),
-    router = express.Router();
-var static = require('serve-static');
-var files = require('./routes/files');
+var express = require('express');
 var debug = require('debug')('raml-serve');
 
 var path = require('path');
 
+// creates dist-override/index.html
 var fs = require('fs');
-fs.readFile(path.join(__dirname, 'node_modules/api-designer/dist/index.html'), 'utf8', function (err,data) {
-  if (!!err) { return console.log(err); }
-  var result = data.replace(/<\/body\>/g, '<script src="angular-persistence.js"></script></body>');
-
-  fs.writeFile('dist-override/index.html', result, 'utf8', function (err) {
-    if (err) return console.log(err);
-    console.log('index.html written');
-  });
-});
+var indexFile = fs.readFileSync(path.join(__dirname, 'node_modules/api-designer/dist/index.html'), 'utf8');
+indexFile = indexFile.replace(/<\/body\>/g, '<script src="angular-persistence.js"></script></body>');
+fs.writeFileSync('dist-override/index.html', indexFile, 'utf8');
 
 function serveOverride (req, res, next) {
   if (req.url === '/index.html' || req.url === '/') {
@@ -36,19 +28,28 @@ function serveOverride (req, res, next) {
   });
 }
 
-router.get('/files/*', files.findAll);
-router.post('/files/*', files.addFile);
-router.put('/files/*', files.updateFile);
-router.delete('/files/*', files.deleteFile);
-router.use('/', serveOverride);
+var ramlServe;
+module.exports = ramlServe = function (ramlPath) {
+  var router = express.Router();
+  var bodyParser = require('body-parser');
+  // quick n dirty = setup RAML_DATAPATH without refactoring files.js
+  process.env.RAML_DATAPATH = ramlPath;
+  var files = require('./routes/files');
 
-module.exports = router;
+  router.use(bodyParser.json());
+  router.get('/files/*', files.findAll);
+  router.post('/files/*', files.addFile);
+  router.put('/files/*', files.updateFile);
+  router.delete('/files/*', files.deleteFile);
+  router.use('/', serveOverride);
+  return router;
+};
 
 if (module.parent === null) {
   var app = express();
-  app.use('/', router);
+  app.use('/', ramlServe('RAML/'));
 
-  var server = app.listen(app.get('port') || 3000, function() {
+  var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Express server listening on ' + server.address().address + ':' + server.address().port + '/');
   });
 }
