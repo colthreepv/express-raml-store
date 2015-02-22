b4810ec242eb2c6af33e95cec7cdde4102d8f761
mrgamer <mister.gamer@gmail.com>
2015-02-22 20:45:51 +0100

    persistence module rework, mostly path issues

--- dist-override/angular-persistence.js
+++ dist-override/angular-persistence.js
@@ -1,7 +1,7 @@
 // gently stolen from: https://github.com/arthurtsang/api-designer/commit/2ad4f3e253b17fa95ea3ea012419d933b098d914
 // just linted and indented
 angular.module('ramlEditorApp')
-.factory('RamlServeFS', function($http, $q, config) {
+.factory('APIStore', function($http, $q, config) {
   var service = {};
 
   function errorFunction(data, status, headers, config) {
@@ -10,67 +10,27 @@ angular.module('ramlEditorApp')
 
   service.directory = function(path) {
     var deferred = $q.defer();
-
-    if (path === '/') {
-      deferred.resolve({
-        path: '/',
-        name: '/',
-        type: 'folder',
-        children: [
-          {
-            path: '/api.raml',
-            name: 'api.raml',
-            type: 'file'
-          },
-          {
-            path: '/test.raml',
-            name: 'test.raml',
-            type: 'file'
-          }
-        ]
-      });
-    }
-    if (path === '/api.raml') {
-      deferred.resolve({
-        path: '/api.raml',
-        name: 'api.raml',
-        type: 'file'
-      });
-    }
-    // $http({
-    //     method: 'GET',
-    //     data: '',
-    //     url: 'files/' + ((path === '/') ? '' : path),
-    //     withCredentials: false
-    //   }).success(function(data) {
-    //     deferred.resolve({
-    //       path: path,
-    //       meta: {},
-    //       children: data
-    //     });
-    //   })
-    //   .error(errorFunction);
+    $http({
+      method: 'GET',
+      url: 'files/' + ((path === '/') ? '' : path),
+      withCredentials: false
+    }).success(function (data) {
+      deferred.resolve(data);
+    })
+    .error(deferred.reject.bind(deferred));
     return deferred.promise;
   };
 
   service.load = function(path, name) {
     var deferred = $q.defer();
-    if (path.endsWith('.meta')) {
-      deferred.resolve('{ "created": "' + Date.now() + '" }');
-    }
-    if (path.endsWith('.raml')) {
-      deferred.resolve('#%RAML 0.8\ntitle: tutored private api\nversion: 0.1.0\nbaseUri: http://localhost:8080/api\nmediaType: application/json\nschemas:\n  - ParseId: |\n      {\n        "type": "string",\n        "minLength": 10,\n        "maxLength": 10\n      }\n  - Lesson: |\n      {\n        "definitions": {\n          "parseId": {\n            "type": "string",\n            "minLength": 10,\n            "maxLength": 10\n          }\n        },\n        "type": "object",\n        "properties": {\n          "tutor": { "$ref": "#/definitions/parseId" },\n          "student": { "$ref": "#/definitions/parseId" }\n        }\n      }\nprotocols: [ HTTP ]\n/lesson:\n  post:\n    description: "creates a new lesson: a student books a lesson with a tutor"\n    body:\n      application/json:\n        schema: Lesson\n        example: |\n          {\n            "tutor": "asd0masd01",\n            "student": "asd0masd02"\n          }\n    responses:\n      200:\n        description: everything went well!\n        body:\n          application/json:\n            example: |\n              { "message": "everything done" }\n');
-    }
-
-    // $http({
-    //     method: 'GET',
-    //     data: '',
-    //     url: 'files/' + path,
-    //     withCredentials: false
-    //   }).success(function(data) {
-    //     deferred.resolve(data[0].content);
-    //   })
-    //   .error(deferred.reject.bind(deferred));
+    $http({
+        method: 'GET',
+        url: 'files' + path,
+        withCredentials: false
+      }).success(function(data) {
+        deferred.resolve(data);
+      })
+      .error(deferred.reject.bind(deferred));
     return deferred.promise;
   };
 
@@ -136,7 +96,7 @@ angular.module('ramlEditorApp')
 
   return service;
 })
-.run(function (RamlServeFS, config) {
-  // Set RamlServeFS as the filesystem to use
-  config.set('fsFactory', 'RamlServeFS');
+.run(function (APIStore, config) {
+  // Set APIStore as the filesystem to use
+  config.set('fsFactory', 'APIStore');
 });
